d81a6d3a2783d612c45f81e7301f6d8d7e01ec02
Songyan Liu <liusongyan04@qq.com>
2025-06-30 00:06:26 +0800

    correct race_planner.cpp

--- src/race_planner/src/race_planner.cpp
+++ src/race_planner/src/race_planner.cpp
@@ -5,34 +5,32 @@ PLUGINLIB_EXPORT_CLASS(race_planner::RacePlanner, nav_core::BaseLocalPlanner)
 
 namespace race_planner
 {
-    class RacePlanner
-    {
-        RacePlanner::RacePlanner()
-        {
-            setlocale(LC_ALL, "");
-        }
-        
-        RacePlanner::~RacePlanner()
-        {}
 
-        void RacePlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
-        {
-            ROS_WARN("RacePlanner initialized with name: %s", name.c_str());
-        }
-        
-        bool RacePlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& plan)
-        {
-            return true;
-        }
-        
-        bool RacePlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
-        {
-            return true;
-        }
-        
-        bool RacePlanner::isGoalReached()
-        {
-            return false;
-        }
-    };   
+    RacePlanner::RacePlanner()
+    {
+        setlocale(LC_ALL, "");
+    }
+    
+    RacePlanner::~RacePlanner()
+    {}
+
+    void RacePlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
+    {
+        ROS_WARN("RacePlanner initialized with name: %s", name.c_str());
+    }
+    
+    bool RacePlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& plan)
+    {
+        return true;
+    }
+    
+    bool RacePlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
+    {
+        return true;
+    }
+    
+    bool RacePlanner::isGoalReached()
+    {
+        return false;
+    }
 }
