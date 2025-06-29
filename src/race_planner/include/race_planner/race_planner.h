a8bbf534070fd2241e4a784f9604d87d3fa45cf0
Songyan Liu <liusongyan04@qq.com>
2025-06-30 00:09:51 +0800

    correct race_planner.h

--- src/race_planner/include/race_planner/race_planner.h
+++ src/race_planner/include/race_planner/race_planner.h
@@ -8,13 +8,14 @@ namespace race_planner
 {
     class RacePlanner : public nav_core::BaseLocalPlanner
     {
-        RacePlanner();
-        ~RacePlanner();
+        public:
+            RacePlanner();
+            ~RacePlanner();
 
-        void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
-        bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan);
-        bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
-        bool isGoalReached();
+            void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
+            bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan);
+            bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+            bool isGoalReached();
     };
 }
 
