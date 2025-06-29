bea24343f4643eb01c581066fbce942fc4ed1291
Songyan Liu <liusongyan04@qq.com>
2025-06-29 23:59:30 +0800

    update race-planner

---
+++ src/race_planner/include/race_planner/race_planner.h
@@ -0,0 +1,21 @@
+#ifndef _RACE_PLANNER_H_
+#define _RACE_PLANNER_H_
+
+#include <ros/ros.h>
+#include <nav_core/base_local_planner.h>
+
+namespace race_planner
+{
+    class RacePlanner : public nav_core::BaseLocalPlanner
+    {
+        RacePlanner();
+        ~RacePlanner();
+
+        void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
+        bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan);
+        bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+        bool isGoalReached();
+    };
+}
+
+#endif
